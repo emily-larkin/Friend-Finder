b9b1c8d8a0b4e16ff5a5f0c6a48d1dd733b7168f
emssslay <emily.robinson@iechs.org>
2019-05-06 13:32:22 -0600

    fixing routes

--- server.js
+++ server.js
@@ -3,13 +3,17 @@ var express = require("express");
 var path = require("path");
 
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({
   extended: true
 }));
 app.use(express.json());
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "/app/public/home.html"));
+  });
 
 // Starts the server to begin listening
 // =============================================================
