fed0f4dc445c51ca88e3105738d2a4f1afc5ca3e
emssslay <emily.robinson@iechs.org>
2019-05-06 13:26:48 -0600

    html pages are up, routing also done

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,12 @@
+app.get("/api/friends", function(req, res) {
+    return res.json(friends);
+  });
+  
+  // posts to tables 
+  app.post("/api/friends", function(req,res) {
+    var newFriend = req.body;
+    console.log(newFriend);
+    newFriend = newFriend.friendName.replace(/\s/g, "").toLowerCase();
+    friends.push(newFriend);
+    res.json(newFriend);
+  })
